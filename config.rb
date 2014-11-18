d095d9effb715417a798cfc6b21ee4c119e5f25f
greatghoul <greatghoul@gmail.com>
2014-11-18 13:31:14 +0800

    add deploy settings

--- config.rb
+++ config.rb
@@ -141,3 +141,14 @@ configure :build do
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
 end
+
+# Deploy
+activate :deploy do |deploy|
+  deploy.build_before = true # default: false
+  deploy.method = :git
+  # Optional Settings
+  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
+  deploy.branch   = 'gitcafe-pages' # default: gh-pages
+  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+end
