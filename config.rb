8d5ae2ddba85936beab599ee6e91bc7ff724c204
greatghoul <greatghoul@gmail.com>
2014-11-18 15:45:15 +0800

    external helpers

--- config.rb
+++ config.rb
@@ -1,4 +1,5 @@
 require "lib/tag_cloud"
+require "lib/blog_helpers"
 
 ###
 # Blog settings
@@ -78,43 +79,11 @@ page "/tags.html"
 # activate :livereload
 
 # Methods defined in the helpers block are available in templates
-helpers do
-  def render_article(article)
-    partial 'article', locals: { article: article }
-  end
+# helpers do
+# end
 
-  def render_article_list(articles, options={})
-    option = { date_format: :short }.merge(options)
-
-    partial 'article_list', locals: { articles: articles, options: options }
-  end
-
-  def render_article_summary(article)
-    html = Nokogiri::HTML(article.summary)
-    html.css('img').each(&:remove)
-    html.css('p:empty').each(&:remove)
-    html.to_s
-  end
-
-  def render_article_tags(article)
-    return '' unless article.tags.any?
-    
-    partial 'article_tags', locals: { tags: article.tags }
-  end
-
-  def render_article_meta(article)
-    partial 'article_meta', locals: { article: article }
-  end
-
-  def to_date(date, format=:default)
-    case format
-    when :short
-      date.strftime('%m-%d')
-    else
-      date.strftime('%Y-%m-%d %H:%M')
-    end
-  end
-end
+# External defined helpers.
+helpers BlogHelpers
 
 set :css_dir, 'stylesheets'
 
