f8aea559a0b59cc1491c936e6ae48c478cf4d72d
greatghoul <greatghoul@gmail.com>
2018-04-11 03:10:39 +0800

    init jekyll and remove middleman and init index page

--- lib/blog_article_ext.rb
+++
@@ -1,8 +0,0 @@
-Middleman::Blog::BlogArticle.module_eval do
-  include Padrino::Helpers::FormatHelpers
-
-  def description
-    text = strip_tags(self.body).gsub(/\s+/, ' ')
-    truncate(text, length: 147)
-  end
-end
