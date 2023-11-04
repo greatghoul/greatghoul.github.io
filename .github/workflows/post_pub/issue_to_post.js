25036e723697b93430c0998dee39a12f973b650a
greatghoul <greatghoul@gmail.com>
2023-11-04 13:21:51 +0000

    fix date.and slug

--- .github/workflows/post_pub/issue_to_post.js
+++ .github/workflows/post_pub/issue_to_post.js
@@ -7,13 +7,16 @@ data.layout = 'post'
 data.title = issue.title
 data.tags = issue.labels.map(x => x.name)
 data.issue = issue.number
-data.slug = String(this.slug || data.issue)
 
-if (!data.date) {
-  const date = new Date(issue.created_at)
-  const dateOptions = { timeZone: "Asia/Shanghai", year: "numeric", month: "2-digit", day: "2-digit" }
-  data.date = date.toLocaleString("zh-CN", dateOptions).replace(/\//g, "-")
+if (data.slug) {
+  data.slug = `${data.issue}-${data.slug}`
+} else {
+  data.slug = `${data.issue}`
 }
+
+const date = data.date || issue.created_at
+const dateOptions = { timeZone: "Asia/Shanghai", year: "numeric", month: "2-digit", day: "2-digit" }
+data.date = date.toLocaleString("zh-CN", dateOptions).replace(/\//g, "-")
 
 const filename = `_posts/${data.date}-${data.slug}.md`
 const filebody = matter.stringify(content, data)
